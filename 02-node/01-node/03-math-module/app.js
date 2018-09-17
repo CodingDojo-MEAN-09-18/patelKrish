60aa07ecd51be4898da1c5836242cfde6d890bf6
Krishna Patel <kpatel1293@gmail.com>
2018-09-17 15:47:11 -0500

    Math Module - DONE

---
+++ 02-node/01-node/03-math-module/app.js
@@ -0,0 +1,6 @@
+var math = require('./mathlib')();
+
+console.log(math.add(2,3));         // return 5
+console.log(math.multiply(3,5));    // return 15
+console.log(math.square(5));        // return 25
+console.log(math.random(1,35));     // return a random number between 1 and 35
