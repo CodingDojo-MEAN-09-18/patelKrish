21e965555eca2be78a96874db4f56dac2b91dbf7
Krishna <kpatel1293@gmail.com>
2018-10-12 23:43:48 -0500

    done pokemon api

---
+++ 05-angular/02-fetch-data/02_API/public/src/app/http.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpService {
+  constructor(private _http: HttpClient) {
+    this.getPokemon();
+  }
+  getPokemon(){
+      let bulbasaur = this._http.get('https://pokeapi.co/api/v2/pokemon/1/');
+      let ability = "";
+      bulbasaur.subscribe( data => {
+        let ans = `${data['name']}'s ablities are: `
+        data['abilities'].forEach(d => { ans += `\n ${d['ability']['name']}`; });
+
+        console.log(ans);
+
+        ability = data['abilities'][1].ability.url;
+
+        let ab = this._http.get(ability);
+
+        ab.subscribe(a => { console.log(`Pokemon with the same ablitiies are: ${a['pokemon'].length}`); })
+      })
+  }
+}
